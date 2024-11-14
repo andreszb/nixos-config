9ceadb66242240e4efe1c35f28217f463a43bd66
Andrés Zambrano <andres@andreszambrano.com>
2024-11-13 19:51:49 -0600

    Update disk-config.nix for Zephy

--- modules/nixos/disk-config.nix
+++ modules/nixos/disk-config.nix
@@ -1,14 +1,13 @@
-_: {
-  # This is the disk layout for a dual-boot system with Windows 10.
+{
   disko.devices = {
-    disk = {
-      nvme0n1 = {
-        device = "/dev/nvme0n1";
+    linux = {
+      nvme1n1 = {
+        device = "/dev/nvme1n1";
         type = "disk";
         content = {
           type = "gpt";
           partitions = {
-            ESP = {
+            boot = {
               type = "EF00";  # EFI partition type.
               size = "500M";
               content = {
@@ -17,9 +16,8 @@ _: {
                 mountpoint = "/boot";
               };
             };
-            root = {
-              start = "901G";  # Start immediately after Windows partition.
-              size = "100%";  # Takes the remaining half of the disk space.
+            nixos = {
+              size = "100%"; 
               content = {
                 type = "filesystem";
                 format = "ext4";
